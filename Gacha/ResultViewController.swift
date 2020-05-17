274a87037627d7a7665b3685972c046b2a3d06c1
cozenakai <masaki2970@icloud.com>
2020-05-18 06:35:04 +0900

    debugged & plus alpha

--- Gacha/ResultViewController.swift
+++ Gacha/ResultViewController.swift
@@ -15,6 +15,7 @@ class ResultViewController: UIViewController {
     
     @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var monsterImageView: UIImageView!
+    var monsterArray: [UIImage]!
     
 
     override func viewDidLoad() {
@@ -23,14 +24,44 @@ class ResultViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         number = Int.random(in: 0...9)
+        
+        monsterArray = [UIImage(named: "monster001.png")!,
+        UIImage(named: "monster002.png")!,
+        UIImage(named: "monster003.png")!,
+        UIImage(named: "monster004.png")!,
+        UIImage(named: "monster005.png")!,
+        UIImage(named: "monster006.png")!,
+        UIImage(named: "monster007.png")!,
+        UIImage(named: "monster008.png")!,
+        UIImage(named: "monster008.png")!,
+        UIImage(named: "monster009.png")!,]
+        
+        monsterImageView.image = monsterArray[number]
+        
         if number == 9 {
             monsterImageView.image = UIImage(named: "monster010")
             backgroundImageView.image = UIImage(named: "bg_gold")
-        }else if number > 7 {
+        }else if number > 6 {
             monsterImageView.image = UIImage(named: "monster006")
             backgroundImageView.image = UIImage(named: "bg_red")
-        }else{
+        }else if number == 5{
+            monsterImageView.image = UIImage(named: "monster005")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+        else if number == 4{
+            monsterImageView.image = UIImage(named: "monster004")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+        else if number == 3{
             monsterImageView.image = UIImage(named: "monster003")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+        else if number == 2{
+            monsterImageView.image = UIImage(named: "monster002")
+            backgroundImageView.image = UIImage(named: "bg_blue")
+        }
+        else{
+            monsterImageView.image = UIImage(named: "monster001")
             backgroundImageView.image = UIImage(named: "bg_blue")
         }
     }
@@ -40,3 +71,4 @@ class ResultViewController: UIViewController {
     }
 
 }
+
